f2e7744ae91238144b37ab9896eebe7da03b0f04
Dany <profedanieljuan@gmail.com>
2024-07-27 21:53:01 -0300

    Se define la clase Persona y se la utiliza en la clase App

--- PrimerasClases/src/App.java
+++ PrimerasClases/src/App.java
@@ -1,5 +1,19 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        
+        Persona persona1 = new Persona();
+        persona1.nombre = "Raul";
+        persona1.apellido = "Alvarez";
+        persona1.edad = 57;
+
+        Persona persona2 = new Persona();
+        persona2.apellido = "Perez";
+        persona2.nombre = "Mariana";
+        persona2.edad = 24;
+
+
+
+        System.out.println(persona1.apellido + "," + persona1.nombre + " tiene "+ persona1.edad + " años");
+        System.out.println(persona2.apellido + "," + persona2.nombre + " tiene "+ persona2.edad + " años");
     }
 }
---
+++ PrimerasClases/src/Persona.java
@@ -0,0 +1,10 @@
+public class Persona {
+
+    // atributos (estados y comportamientos de un objeto)
+    String nombre;
+    String apellido;
+    int edad;
+    
+
+
+}
