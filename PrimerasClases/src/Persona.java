a85dadedece9d0fd0065e3b471f723a38afd563c
Dany <profedanieljuan@gmail.com>
2024-07-27 22:19:14 -0300

    Se agregan métodos a la clase Persona

--- PrimerasClases/src/Persona.java
+++ PrimerasClases/src/Persona.java
@@ -1,10 +1,17 @@
 public class Persona {
 
-    // atributos (estados y comportamientos de un objeto)
+    // atributos (estados y caracteristicas de un objeto)
     String nombre;
     String apellido;
     int edad;
     
+    // Métodos (comportamientos de un objeto)
 
+    public String darNombreCompleto(){
+            return apellido + ", " + nombre;
 
+    }
+    public String enviarSaludo(String saludado){
+        return "hola, como estas " + saludo + "?";
+    } 
 }
