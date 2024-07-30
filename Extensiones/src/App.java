d7b8592d9ed599f00ba56a4a2e1b50103225bee9
ElNuevoAprendiz! <profedanieljuan@gmail.com>
2024-07-30 10:53:48 -0300

    Se comienza a trabajar con excepciones

---
+++ Extensiones/src/App.java
@@ -0,0 +1,7 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        // Se comienza a trabajar con las excepciones unchecked (no verificadas)
+
+        
+    }
+}
