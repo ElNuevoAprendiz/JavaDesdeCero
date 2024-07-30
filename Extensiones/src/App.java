c681dd7457671f324e0fc31b59adbd8bcc82d4c6
ElNuevoAprendiz! <profedanieljuan@gmail.com>
2024-07-30 11:05:23 -0300

    El bucle try catch indica que va a intertar (try) hacer algo, si hubiese un error lo podemos manejar con el catch. De esta forma no se va a interrumpir el flujo de trabajo aunque si vamos a mostrar el error.

--- Extensiones/src/App.java
+++ Extensiones/src/App.java
@@ -2,6 +2,9 @@ public class App {
     public static void main(String[] args) throws Exception {
         // Se comienza a trabajar con las excepciones unchecked (no verificadas)
 
-        
+        int nuro1 =5;
+        int nro2 = 0;
+
+        System.out.println(nuro1/nro2);
     }
 }
