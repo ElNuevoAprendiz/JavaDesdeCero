085c1f556622442a7b36056cfdb4c7e543111b46
Dany <profedanieljuan@gmail.com>
2024-08-18 14:57:59 -0300

    Sincronizacion w11

--- Colecciones/src/arrays.java
+++ Colecciones/src/arrays.java
@@ -24,12 +24,7 @@ public class arrays {
             tipo_de_dato[] nombre_array = new tipo_de_dato[] {valor1, valor2, valor3, ...};
 
 
-        ¡Hola! Con gusto te explicaré las diferentes formas de definir un array en Java, como si fuera tu profesor particular.
-
-¿Qué es un array en Java?
-
-Imagina un array como una caja con varios compartimentos, todos del mismo tamaño. Cada compartimento puede almacenar un valor del mismo tipo (por ejemplo, números enteros, decimales, letras, etc.). Los arrays son muy útiles cuando necesitas almacenar una colección de datos relacionados.
-
+       
 Formas de definir un array en Java:
 
     Declaración y creación:
@@ -38,38 +33,28 @@ Formas de definir un array en Java:
 
         tipo_de_dato[] nombre_array;
 
-        Usa el código con precaución.
 
 Esto simplemente indica que vas a crear un array de un tipo de dato específico. Por ejemplo:
-Java
 
 int[] numeros;
 String[] nombres;
 
-Usa el código con precaución.
 Creación:
-Java
 
 nombre_array = new tipo_de_dato[tamaño];
 
-Usa el código con precaución.
 Aquí asignas memoria para el array y especificas el número de elementos que puede contener. Por ejemplo:
-Java
 
 numeros = new int[10]; // Un array de 10 enteros
 nombres = new String[5]; // Un array de 5 cadenas de texto
 
-Usa el código con precaución.
-
 Declaración, creación e inicialización en una sola línea:
-Java
 
 tipo_de_dato[] nombre_array = new tipo_de_dato[] {valor1, valor2, valor3, ...};
 
-Usa el código con precaución.
 
 Esta forma es más compacta y te permite inicializar el array con valores directamente. Por ejemplo:
-Java
+
 
 int[] edades = {25, 30, 22, 35};
 
@@ -108,7 +93,7 @@ public class EjemploArrays {
     }
 }
 
-Usa el código con precaución.
+
 
 Puntos importantes a recordar:
 
